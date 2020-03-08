e20f3fb9b5d589fc72bbc8b9cd021189df3554cd
zanjie1999 <zanjie1999@yeah.net>
2020-03-08 22:28:19 +0800

    OK QwQ

--- esp_duck/webserver.cpp
+++ esp_duck/webserver.cpp
@@ -1,9 +1,3 @@
-/*
-   Copyright (c) 2019 Stefan Kremser
-   This software is licensed under the MIT License. See the license file for details.
-   Source: github.com/spacehuhn/WiFiDuck
- */
-
 #include "webserver.h"
 
 #include <ESP8266WiFi.h>
@@ -18,6 +12,7 @@
 #include "cli.h"
 #include "spiffs.h"
 #include "settings.h"
+#include "com.h"
 
 #include "webfiles.h"
 
